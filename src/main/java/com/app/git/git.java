e69f23b30fd3c8bd9a04f0dc871feff67669ef05
huojia <huojia>
2019-06-01 19:10:25 +0800

    new git-test

---
+++ src/main/java/com/app/git/git.java
@@ -0,0 +1,28 @@
+/**
+ * @项目名：git-01-test
+ * @创建人： hjwzz
+ * @创建时间： 2019-06-01
+ * @公司： www.huojia.com
+ * @描述：TODO
+ */
+package com.app.git;
+
+/**
+ * <p>NAME: git</p>
+ * @author hjwzz
+ * @date 2019-06-01 19:07:42
+ * @version 1.0
+ */
+public class git {
+    /**
+    * @Author hjwzz
+    * @Description //TODO
+    * @Date 2019/6/1 19:08
+    * @Param
+    * @return
+    */
+
+    public static void main(String[] args) {
+        System.out.println("git");
+    }
+}
